85d03972991d7954e2ed64053a32d047380f657f
CrysteonSnowen <39706271+CrysteonSnowen@users.noreply.github.com>
2018-07-28 15:15:31 -0500

    Update lovebirbbot.js

--- lovebirbbot.js
+++ lovebirbbot.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client;
 
-client.setMaxListeners(50);
-
 //Check if the bot is online
 client.on("ready", () => { //The Ready Event itself!
     console.log("READY"); //A simple console log to say that the bot is online and ready.
@@ -10,6 +8,7 @@ client.on("ready", () => { //The Ready Event itself!
 
 //Change its token ID everytime it disconnects or settings are changed.
 client.login(process.env.BOT_TOKEN);
+client.setMaxListeners(50);
 
 //================================  COMMANDS  =========================================
 //========= !compliment + mentioned user ===========
